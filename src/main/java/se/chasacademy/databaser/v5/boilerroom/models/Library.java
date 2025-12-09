0cc89870d830d4cf15b37b0f6244b03bd816f4c6
Joakim Englund <joakimenglund@protonmail.com>
2025-12-09 10:19:27 +0100

    Added Library class

---
+++ src/main/java/se/chasacademy/databaser/v5/boilerroom/models/Library.java
@@ -0,0 +1,30 @@
+package se.chasacademy.databaser.v5.boilerroom.models;
+
+public class Library {
+    private int library_id;
+    private String name, address;
+
+    public int getLibrary_id() {
+        return library_id;
+    }
+
+    public void setLibrary_id(int library_id) {
+        this.library_id = library_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
