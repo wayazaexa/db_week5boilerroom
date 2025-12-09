af79495e5635f06726b481144ff877ee4dd4a31a
Joakim Englund <joakimenglund@protonmail.com>
2025-12-09 10:23:11 +0100

    Added Loan class

---
+++ src/main/java/se/chasacademy/databaser/v5/boilerroom/models/Loan.java
@@ -0,0 +1,32 @@
+package se.chasacademy.databaser.v5.boilerroom.models;
+
+import java.time.LocalDate;
+
+public class Loan {
+    private int loan_id;
+    LocalDate start_date, end_date;
+
+    public int getLoan_id() {
+        return loan_id;
+    }
+
+    public void setLoan_id(int loan_id) {
+        this.loan_id = loan_id;
+    }
+
+    public LocalDate getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(LocalDate start_date) {
+        this.start_date = start_date;
+    }
+
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(LocalDate end_date) {
+        this.end_date = end_date;
+    }
+}
