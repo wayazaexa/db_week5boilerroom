5c0cbef70e22067ee56bef7d5476b6962f896fc8
Joakim Englund <joakimenglund@protonmail.com>
2025-12-09 10:14:45 +0100

    Added Author class

---
+++ src/main/java/se/chasacademy/databaser/v5/boilerroom/models/Author.java
@@ -0,0 +1,22 @@
+package se.chasacademy.databaser.v5.boilerroom.models;
+
+public class Author {
+    private int author_id;
+    private String name;
+
+    public int getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(int author_id) {
+        this.author_id = author_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
