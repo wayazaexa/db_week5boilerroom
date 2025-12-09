03df2a70a57acac20edf6adc208c9d24f52ca004
Joakim Englund <joakimenglund@protonmail.com>
2025-12-09 10:24:13 +0100

    Added Member class

---
+++ src/main/java/se/chasacademy/databaser/v5/boilerroom/models/Member.java
@@ -0,0 +1,30 @@
+package se.chasacademy.databaser.v5.boilerroom.models;
+
+public class Member {
+    private int member_id;
+    private String name, email;
+
+    public int getMember_id() {
+        return member_id;
+    }
+
+    public void setMember_id(int member_id) {
+        this.member_id = member_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
